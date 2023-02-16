764cac77b882d051ab4e7de3f03d74afa4f52f5e
MagdalenaBernat <magdalena.bernat@onet.pl>
2023-02-16 02:50:38 +0100

    pagination, readme

--- src/index.js
+++ src/index.js
@@ -8,4 +8,5 @@ import {
 import { renderModal } from './js/movie-modal';
 import { home, library } from './js/home-library-switcher';
 import { addSpinner, removeSpinner } from './js/spinner';
-import { createSearchList, searchMovies, fetchMovies} from './js/search-movies';
+import { createSearchList, searchMovies, fetchMovies } from './js/search-movies';
+import { pagination, setPaginationBtns, clearPageContent, createPagination } from './js/pagination';
